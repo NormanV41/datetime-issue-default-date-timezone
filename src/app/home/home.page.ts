86bcebcd471935902ee9690ddcf8731e8ec0a076
Norman Vasquez <nrvasquez41@hotmail.com>
2022-01-09 21:01:35 -0600

    This commit shows how IonDatetime presents inconsistencies with timezones.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public timezoneOffset = '2022-01-06T22:00:00.000-06:00';
+  public utcTimezone = '2022-01-07T04:00:00.000Z';
+  public equalityMessage = '';
 
-  constructor() {}
-
+  constructor() {
+    const timezoneOffsetDate = new Date(this.timezoneOffset);
+    const utcTimezoneDate = new Date(this.utcTimezone);
+    if (timezoneOffsetDate.getTime() === utcTimezoneDate.getTime()) {
+      this.equalityMessage = 'for javascript this two dates are equal.';
+    }
+  }
 }
